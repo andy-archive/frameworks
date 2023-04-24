eee2a34bc8be2fd03fc62733e433eaafa3ce3ba5
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-04-25 00:30:21 +0900

    Feat: ZStack으로 그라데이션 구현
    - CMD + SHIFT + L 단축키로 library 단축키 사용
    - HStack, VStack 적용 및 삭제
    - ZStack 적용
    - LinearGradient를 이용해 그라데이션 배경 구현

--- SwiftUI/MyFirstSwiftUI/MyFirstSwiftUI/ContentView.swift
+++ SwiftUI/MyFirstSwiftUI/MyFirstSwiftUI/ContentView.swift
@@ -4,12 +4,26 @@
 //
 //  Created by Taekwon Lee on 2023/04/23.
 
+// This is a SwiftUI Tutorial from Sean Allen
+// https://www.youtube.com/watch?v=HXoVSbwWUIk&t=15s
+
 import SwiftUI
 
+// Open Preview Shortcut: CMD OPTION ENTER
+
+// Open Library: CMD SHIFT L
+
+// VStack is Vertical Stack
+// HStack is Horizontal Stack
+// ZStack is Z-axis Stack -> depth or distance from the screen
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .yellow, .orange]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
