4c94897f32ff9c3c2ecb885721846101b3c44317
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-04-25 00:27:50 +0900

    project setting

---
+++ SwiftUI/MyFirstSwiftUI/MyFirstSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MyFirstSwiftUI
+//
+//  Created by Taekwon Lee on 2023/04/23.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/MyFirstSwiftUI/MyFirstSwiftUI/MyFirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIApp.swift
+//  MyFirstSwiftUI
+//
+//  Created by Taekwon Lee on 2023/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
