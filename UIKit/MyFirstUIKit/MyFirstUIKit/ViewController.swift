34937d3ec17dac9a9faad0112a6c9266ad474e25
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-05-11 13:09:31 +0900

    Feat: [UIKit/MyFirstUIKit] Label, Button 생성 및 속성 조절
    - Library에서 Label과 Button을 각각 드래그
    - 각 속성의 크기 조절
    - Constraints 조절
        - Align Top/Botoom to Safe Area
        - Horizontally in Container
    - control + drag로 IBOutlet + IBAction 지정
    - UILabel에 대한 속성으로 버튼 클릭 시 이벤트 지정
        - .text
        - .backgroundColor
        - .textColor
        - .textAlignment

--- UIKit/MyFirstUIKit/MyFirstUIKit/ViewController.swift
+++ UIKit/MyFirstUIKit/MyFirstUIKit/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController { // ViewController 클래스
 
-    override func viewDidLoad() {
+    // control + drag
+    @IBOutlet weak var mainLabel: UILabel! // 변수 선언
+    
+    override func viewDidLoad() { // 함수 실행
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    // Type: UIButton
+    // Event: Touch Up Inside
+    @IBAction func pressedButton(_ sender: UIButton) {
+        // 버튼 클릭 시 UILabel!.text를 변경
+        mainLabel.text = "Welcome!"
+        
+        // 버튼 클릭 시 UILabel!.backgroundColor를 변경
+//        mainLabel.backgroundColor = UIColor.gray
+        
+        // colorLiteral 대신 #colorLiteral(
+        mainLabel.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        
+        // #imageLiteral(
+        
+        mainLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        // option key + trackpad click -> 해당 속성에 대한 정의 조회
+        mainLabel.textAlignment = NSTextAlignment.right
+    }
+    
 }
 
