9205ebf1e6b87c2c8c7b2868bf98d4a82161ecbc
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-05-11 22:52:52 +0900

    Update: [UIKit/MyFirstUIKit] mainButton 변수 추가 및 IBAction에 속성 추가
    - UIButton을 드래그하여 IBOutlet의 변수로 mainButton 추가
    - IBAction에 mainButton의 속성 추가
        - setTitleColor로 버튼 클릭 시 텍스트 색상 변경

--- UIKit/MyFirstUIKit/MyFirstUIKit/ViewController.swift
+++ UIKit/MyFirstUIKit/MyFirstUIKit/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 class ViewController: UIViewController { // ViewController 클래스
 
     // control + drag로 생성
-    // Attribute: Interface Builder의 Outlet (배출할 수단)
+    // Attribute: Interface Builder의 Outlet(배출할 수단)
     @IBOutlet weak var mainLabel: UILabel! // 변수 선언
+    
+    @IBOutlet weak var mainButton: UIButton!
     
     // 앱 실행 시 초기 화면을 나타내는 함수
     override func viewDidLoad() { // 함수 실행
@@ -23,7 +25,7 @@ class ViewController: UIViewController { // ViewController 클래스
 
     }
 
-    // Interface Builder의 Action 
+    // Interface Builder의 Action(동작) : 버튼 클릭 시 속성 변경
     // Type: UIButton
     // Event: Touch Up Inside
     @IBAction func pressedButton(_ sender: UIButton) {
@@ -35,6 +37,9 @@ class ViewController: UIViewController { // ViewController 클래스
         
         // colorLiteral 대신 #colorLiteral(
         mainLabel.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        
+        mainButton.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        mainButton.setTitleColor(UIColor.black, for: UIControl.State.normal)
         
         // #imageLiteral(
         
