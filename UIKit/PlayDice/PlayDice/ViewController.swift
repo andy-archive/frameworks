d06154aecae3a12f306b4f65bc6a4eed4c2da42e
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-05-12 17:37:50 +0900

    Feat: [UIKit/PlayDice] 버튼 클릭 시 주사위 이미지 랜덤 출력
    - 주사위 이미지 Assets.xcassets에 생성
    - IBOutlet으로 두 가지 UIImageView 생성
    - diceArray로 주사위 이미지에 대한 배열 할당
    - IBAction으로 버튼 클릭 시 주사위 배열의 랜덤 요소 할당

--- UIKit/PlayDice/PlayDice/ViewController.swift
+++ UIKit/PlayDice/PlayDice/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leftDiceView: UIImageView!
+    @IBOutlet weak var rightDiceView: UIImageView!
+    
+    let diceArray: [UIImage] = [#imageLiteral(resourceName: "dice1"), #imageLiteral(resourceName: "dice2"), #imageLiteral(resourceName: "dice3"), #imageLiteral(resourceName: "dice4"), #imageLiteral(resourceName: "dice5"), #imageLiteral(resourceName: "dice6")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func TappedRollButton(_ sender: UIButton) {
+        // 버튼 클릭 시 왼쪽 이미지 랜덤 변경
+        leftDiceView.image = diceArray.randomElement()
+        
+        // 버튼 클릭 시 오른쪽 이미지 랜덤 변경
+        rightDiceView.image = diceArray.randomElement()
+    }
 }
 
