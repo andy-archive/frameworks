3914c12a653d70be56236b2df52f43e60f01e323
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-05-13 22:37:36 +0900

    Add: [UIKit/RockPaperScissors] IBOutlet, IBAction 생성

--- UIKit/RockPaperScissors/RockPaperScissors/ViewController.swift
+++ UIKit/RockPaperScissors/RockPaperScissors/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var computerImageView: UIImageView!
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    @IBOutlet weak var computerChoiceLabel: UILabel!
+    @IBOutlet weak var userChoiceView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func rpsButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func selectButtonTapped(_ sender: Any) {
+    }
 }
 
